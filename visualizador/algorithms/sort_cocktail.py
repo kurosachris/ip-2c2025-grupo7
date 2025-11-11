60d9ecce79733b206c20bf8b13433959940c28c3
Christian Laureano <laurenochris@gmail.com>
2025-11-11 00:13:49 -0300

    Update sort_cocktail.py

--- visualizador/algorithms/sort_cocktail.py
+++ visualizador/algorithms/sort_cocktail.py
@@ -20,11 +20,42 @@ def init(vals):
 
 def step():
     global items, n, i, j, izq, der, tecla
-
+    
     if izq >= der:
         return {"done": True}
     
-    #pasada externa:
+    if tecla:
+        swap = False
+        a = j
+        b = j + 1
+        if items[a] > items[b]:
+            items[a], items[b] = items[b], items[a]
+            swap = True
+            j += 1
+            if j >= der:
+                j = der - 1
+                der -= 1
+                tecla = False
+                i =+ 1
+    
+    else:
+        a = j
+        b = j - 1
+        if items[a] < items[b]:
+            items[a], items[b] = items[b], items[a]
+            swap = True
+            j -= 1
+            if j <= izq:
+                j = izq
+                izq += 1
+                tecla = True
+                i += 1
+    return {"a":a, "b":b, "swap": swap, "done":False}
+
+
+    
+
+
     
     
 
