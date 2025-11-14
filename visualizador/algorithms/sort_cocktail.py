6f84492f4822d3eba190b5fc5571b8eeb7db6dd9
Christian Laureano <laurenochris@gmail.com>
2025-11-14 18:53:46 -0300

    correcciones
    me di cuenta que la variable "i" no tenía uso, así que la descarté

--- visualizador/algorithms/sort_cocktail.py
+++ visualizador/algorithms/sort_cocktail.py
@@ -2,7 +2,6 @@
 
 items = []
 n = 0
-i = 0
 j = 0
 izq = 0
 der = 0
@@ -12,14 +11,13 @@ def init(vals):
     global items, n, i, j, izq, der, tecla
     items = list(vals)
     n = len(items)
-    i = 0
     j = 0
     izq = 0
     der = n - 1
     tecla = True
 
 def step():
-    global items, n, i, j, izq, der, tecla
+    global items, n, j, izq, der, tecla
     
     if izq >= der:
         return {"done": True}
@@ -94,33 +92,3 @@ def step():
                 if izq >= der:
                     return {"done": True}
     return {"a":a, "b":b, "swap": swap, "done":False}
-
-
-    
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    # TODO:
-    # 1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
-    # 2) Si corresponde, hacer el intercambio real en items[a], items[b] y marcar swap=True.
-    # 3) Avanzar punteros (preparar el próximo paso).
-    # 4) Devolver {"a": a, "b": b, "swap": swap, "done": False}.
-    #
-    # Cuando no queden pasos, devolvé {"done": True}.
-    return {"done": True}
