8ac6a14db882005c0552f706d50a04315196c1db
Christian Laureano <laurenochris@gmail.com>
2025-11-14 22:13:23 -0300

    ultimos detalles
    Optimización del código y definiciones más concisas

--- visualizador/algorithms/sort_cocktail.py
+++ visualizador/algorithms/sort_cocktail.py
@@ -1,94 +1,50 @@
-# Contrato: init(vals), step() -> {"a": int, "b": int, "swap": bool, "done": bool}
-
-items = []
-n = 0
+items = []     
+n = 0   
 j = 0
-izq = 0
-der = 0
-tecla = True
+izq = 0    #define el límite izquierdo               
+der = 0    #define el límite derecho
+tecla = True    #es el "switch" que controla la dirección del recorrido
 
 def init(vals):
-    global items, n, i, j, izq, der, tecla
+    global items, n, j, izq, der, tecla
     items = list(vals)
     n = len(items)
     j = 0
-    izq = 0
-    der = n - 1
-    tecla = True
+    izq = 0    #el límite izquierdo inicia en el primer elemento de la lista
+    der = n - 1    #el límite derecho inicia en el último elemento de la lista
+    tecla = True    
 
 def step():
     global items, n, j, izq, der, tecla
-    
-    if izq >= der:
+
+    if izq >= der:         #si los límites del recorrido se cruzan, el programa termina
         return {"done": True}
-    
+
     swap = False
-    a = j
-    b = j + 1
     
-    if tecla:
+    if tecla:    #recorrido de izquierda a derecha
+        a = j 
+        b = j + 1
+
+        if items[a] > items[b]:
+            items[a], items[b] = items[b], items[a]
+            swap = True
+        j += 1 
+
         if j >= der:
-            der -= 1
             tecla = False
-            j = der
-            if izq >= der:
-                return {"done": True}
-            a = j
-            b = j - 1
-            if items[a] < items[b]:
-                items[a], items[b] = items[b], items[a]
-                swap = True
-            j -= 1
-            if j <= izq:
-                izq += 1
-                tecla = True
-                j = izq
-                if izq >= der:
-                    return {"done": True}
-        else:
-            a = j
-            b = j + 1
-            if items[a] > items[b]:
-                items[a], items[b] = items[b], items[a]
-                swap = True
-            j += 1
-            if j >= der:
-                der -= 1
-                tecla = False
-                j = der
-                if izq >= der:
-                    return {"done": True}
-    
-    else:
+            der -= 1   #establece el nuevo límite derecho
+
+    else:     #recorrido de derecha a izquierda
         a = j
         b = j - 1
-        if j <= izq:
-            izq += 1
+        if items[a] < items[b]:
+            items[a], items[b] = items[b], items[a]
+            swap = True
+        j -= 1    
+
+        if j<=izq:
             tecla = True
-            j = izq
-            if izq >= der:
-                return {"done": True}
-            a = j
-            b = j + 1
-            if items[a] > items[b]:
-                items[a], items[b] = items[b], items[a]
-                swap = True
-            j += 1
-            if j >= der:
-                der -= 1
-                tecla = False
-                j = der
-                if izq >= der:
-                    return {"done": True}
-        else:
-            if items[a] < items[b]:
-                items[a], items[b] = items[b], items[a]
-                swap = True
-            j -= 1
-            if j <= izq:
-                izq += 1
-                tecla = True
-                j = izq
-                if izq >= der:
-                    return {"done": True}
-    return {"a":a, "b":b, "swap": swap, "done":False}
+            izq += 1     #establece el nuevo límite izquierdo
+
+    return {"a": a, "b": b, "swap": swap, "done": False}          
